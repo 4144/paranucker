fc20459eaecf66ff6c6631b4b012de69860c4a65
Andrei Karas <akaras@inbox.ru>
2015-06-07 13:43:52 +0300

    Move actual test invoke commands to make file.

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd test
-g++-5 -fplugin=../build/src/.libs/libastdumper.so test1.c 2>../test.txt
+cd build
+make test
 
-cd ..
+#cd test
+#g++-5 -fplugin=../build/src/.libs/libastdumper.so test1.c 2>../test.txt
+
+#cd ..
