cb33d50270c12f3f293aabefae0b7fbb4f1dbc13
Andrei Karas <akaras@inbox.ru>
2015-06-09 15:14:15 +0300

    Remove from test4.c function main. Update test results.

--- test/test4.c
+++ test/test4.c
@@ -1,8 +1,3 @@
-int main(void)
-{
-    return 0;
-}
-
 void func4_1(int a, int *b, char *c);
 void func4_2(int a, int *b, char *c) __attribute__((nonnull (2)));
 void func4_3(int a, int *b, char *c) __attribute__((nonnull (2, 3)));
