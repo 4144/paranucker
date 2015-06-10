c6b216fd52a2ee6c13312ed35236c3e5cc3bcb7f
Andrei Karas <akaras@inbox.ru>
2015-06-10 19:50:01 +0300

    Update tests and test results.

--- test/test6.c
+++ test/test6.c
@@ -1,6 +1,9 @@
 void func1(int *a);
 void func2(int *a);
 void func3(int *a);
+void func4(int *a);
+void func5(int *a);
+void func6(int *a);
 
 void func1(int *a)
 {
@@ -21,4 +24,32 @@ void func3(int *a)
         return;
 
     *a = 300;
+}
+
+void func4(int *a)
+{
+    if (a)
+    {
+        *a = 400;
+    }
+}
+
+void func5(int *a)
+{
+    if (a)
+    {
+        *a = 500;
+    }
+    *a = 600;
+}
+
+void func6(int *aptr)
+{
+    if (!aptr)
+    {
+        int k = 10;
+        return;
+    }
+
+    *aptr = 700;
 }
