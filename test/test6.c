7686193dce3f165438f5f1cda041737db6919e99
Andrei Karas <akaras@inbox.ru>
2015-06-11 23:43:32 +0300

    Update test6 and result.

--- test/test6.c
+++ test/test6.c
@@ -4,6 +4,8 @@ void func3(int *a);
 void func4(int *a);
 void func5(int *a);
 void func6(int *a);
+void func7(int *a);
+void func8(int *a);
 
 void func1(int *a)
 {
@@ -43,13 +45,42 @@ void func5(int *a)
     *a = 600;
 }
 
-void func6(int *aptr)
+void func6(int *a)
 {
-    if (!aptr)
+    if (!a)
     {
         int k = 10;
         return;
     }
 
-    *aptr = 700;
+    *a = 700;
+}
+
+void func7(int *aptr)
+{
+    if (1 == 0)
+    {
+        if (!aptr)
+        {
+            int k = 10;
+            return;
+        }
+    }
+
+    *aptr = 800;
+}
+
+void func8(int *aptr)
+{
+    if (!aptr)
+    {
+        if (1 == 0)
+        {
+            int k = 10;
+            int l = 20;
+        }
+        return;
+    }
+
+    *aptr = 900;
 }
