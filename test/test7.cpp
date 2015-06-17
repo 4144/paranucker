a23d3c114365ab80ccfc3194db887e258dbec77d
Andrei Karas <akaras@inbox.ru>
2015-06-17 15:51:38 +0300

    Update test7. Update test results.

--- test/test7.cpp
+++ test/test7.cpp
@@ -1,3 +1,17 @@
+#include <string>
+
+struct Struct1
+{
+    Struct1() :
+        str(),
+        num(0)
+    {
+    }
+
+    std::string str;
+    int num;
+};
+
 class Object1
 {
     int k;
@@ -19,5 +33,17 @@ class Object1
     void func4(int *aptr)
     {
         k = 3;
+    }
+
+    void func5(Struct1 *sptr)
+    {
+        sptr->num = 0;
+    }
+
+    void func6(Struct1 *sptr)
+    {
+        if (!sptr)
+            return;
+        sptr->num = 0;
     }
 };
