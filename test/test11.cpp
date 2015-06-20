f9297a22459a24b6909d89bd5491f9e76062b82a
Andrei Karas <akaras@inbox.ru>
2015-06-20 21:08:33 +0300

    Update test 11. Update test results.

--- test/test11.cpp
+++ test/test11.cpp
@@ -10,22 +10,64 @@ class Object1
 
     void func1(Data1 *ptr1, Data1 *ptr2)
     {
-        if (!ptr1 || ptr1->val == 1)
+        if (!ptr1 || ptr1->val == 100)
         {
-            return;
-        }
-
-        ptr1->val = 100;
-    }
-
-    void func2(Data1 *ptr1, Data1 *ptr2)
-    {
-        if (ptr1 && ptr1->val == 1)
-        {
-            ptr1->val = 100;
             return;
         }
 
         ptr1->val = 200;
+    }
+
+    void func2(Data1 *ptr1, Data1 *ptr2)
+    {
+        if (ptr1 && ptr1->val == 100)
+        {
+            ptr1->val = 200;
+            return;
+        }
+
+        ptr1->val = 300;
+    }
+
+    void func3(Data1 *ptr1, Data1 *ptr2)
+    {
+        if (ptr1 || ptr1->val == 100)
+        {
+            return;
+        }
+
+        ptr1->val = 200;
+    }
+
+    void func4(Data1 *ptr1, Data1 *ptr2)
+    {
+        if (!ptr1 && ptr1->val == 100)
+        {
+            return;
+        }
+
+        ptr1->val = 200;
+    }
+
+    void func5(Data1 *ptr1, Data1 *ptr2)
+    {
+        if ((!ptr1 || ptr1->val == 100) || (!ptr2 || ptr2->val == 200))
+        {
+            return;
+        }
+
+        ptr1->val = 300;
+        ptr2->val = 400;
+    }
+
+    void func6(Data1 *ptr1, Data1 *ptr2)
+    {
+        if ((!ptr1 || ptr1->val == 100) && (!ptr2 || ptr2->val == 200))
+        {
+            return;
+        }
+
+        ptr1->val = 300;
+        ptr2->val = 400;
     }
 };
