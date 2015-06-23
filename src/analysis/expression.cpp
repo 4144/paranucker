5b246024eb964756df6e8bc6ae527f259643c38f
Andrei Karas <akaras@inbox.ru>
2015-06-23 16:51:52 +0300

    Move TRUTH_ANDIF_EXPR analysis into shared function.

--- src/analysis/expression.cpp
+++ src/analysis/expression.cpp
@@ -185,23 +185,19 @@ void analyseTruthOrIfExpr(TruthOrIfExprNode *node, const WalkItem &wi, WalkItem 
     Log::dumpWI(node, "wo out ", wo);
 }
 
-void analyseTruthAndIfExpr(TruthAndIfExprNode *node, const WalkItem &wi, WalkItem &wo)
+void analyseAndCondition(Node *node, Node *node1, Node *node2, const WalkItem &wi, WalkItem &wo)
 {
-    // need two args for check
-    if (node->args.size() < 2 || command == FindArgs)
-        return;
-
     Log::dumpWI(node, "wo in ", wo);
     WalkItem wo1 = wo;
     WalkItem wo2 = wo;
-    walkTree(node->args[0], wi, wo1);
+    walkTree(node1, wi, wo1);
     Log::dumpWI(node, "wo1 ", wo1);
     WalkItem wi2 = wi;
     removeCheckNullVarsSet(wi2, wo1.checkedNonNullVars);
     wi2.checkNullVars.insert(wo1.checkedNullVars.begin(),
         wo1.checkedNullVars.end());
     Log::dumpWI(node, "wi2 ", wi2);
-    walkTree(node->args[1], wi2, wo2);
+    walkTree(node2, wi2, wo2);
     Log::dumpWI(node, "wo2 ", wo2);
 
     wo.stopWalking = true;
@@ -221,6 +217,15 @@ void analyseTruthAndIfExpr(TruthAndIfExprNode *node, const WalkItem &wi, WalkIte
     wo.cleanExpr = wo1.cleanExpr && wo2.cleanExpr;
     wo.uselessExpr = wo1.uselessExpr && wo2.uselessExpr;
     Log::dumpWI(node, "wo out ", wo);
+}
+
+void analyseTruthAndIfExpr(TruthAndIfExprNode *node, const WalkItem &wi, WalkItem &wo)
+{
+    // need two args for check
+    if (node->args.size() < 2 || command == FindArgs)
+        return;
+
+    analyseAndCondition(node, node->args[0], node->args[1], wi, wo);
 }
 
 // args[0]  condition
