acc94996b12758248d8ce991a71ecc703efd85cf
Andrei Karas <akaras@inbox.ru>
2015-06-26 23:52:46 +0300

    Add test 24.

---
+++ test/test24.c
@@ -0,0 +1,51 @@
+struct Data1;
+
+typedef void (*FuncPtr1) (void);
+typedef void (*FuncPtr2) (struct Data1 *ptr);
+
+struct Data1
+{
+    int val;
+    struct Data1 *ptrval;
+    int arr[3];
+    FuncPtr2 fptr1;
+};
+
+int ftest1(void)
+{
+    return 1;
+}
+
+static int ftest2(struct Data1 *ptr0)
+{
+    return 1;
+}
+
+void ftest3(struct Data1 *ptr0)
+{
+}
+
+int k;
+
+void func1(FuncPtr1 ptr1)
+{
+    ptr1();
+}
+
+void func2(struct Data1 *ptr1)
+{
+    FuncPtr2 fptr2;
+    fptr2(ptr1);
+}
+
+void func3(struct Data1 *ptr1)
+{
+    struct Data1 *data;
+    data->fptr1(ptr1);
+}
+
+void func4(struct Data1 *ptr1)
+{
+    struct Data1 *ptr2;
+    ptr1->fptr1(ptr2);
+}
