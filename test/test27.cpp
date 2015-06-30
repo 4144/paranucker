d1a21baa5abad68ac83cad2206976628cce4d570
Andrei Karas <akaras@inbox.ru>
2015-07-01 02:43:53 +0300

    Update test 27. Update test results.

--- test/test27.cpp
+++ test/test27.cpp
@@ -77,4 +77,108 @@ class Object1
         tmp1->val = 800;
         tmp2->val = 900;
     }
+
+    void func3(Data1 *ptr1)
+    {
+        Data1 *ptr2;
+        ptr1->val = 100;
+        ptr1 = ptr2;
+        ptr2->val = 200;
+        if (!ptr1)
+        {
+            ptr1->val = 300;
+        }
+        ptr1->val = 400;
+    }
+
+    void func4(Data1 *const ptr1)
+    {
+        Data1 *ptr2;
+        tmp1 = ptr1;
+        tmp1 = ptr2;
+
+        if (tmp1)
+        {
+            ptr1->val = 100;
+            tmp1->val = 200;
+        }
+        else
+        {
+            ptr1->val = 300;
+            tmp1->val = 400;
+        }
+        ptr1->val = 500;
+        tmp1->val = 600;
+    }
+
+    void func5(Data1 *ptr1, Data1 *ptr3)
+    {
+        Data1 *ptr2;
+        ptr1->val = 100;
+        ptr1 = ptr2;
+        ptr2->val = 200;
+        if (!ptr1)
+        {
+            ptr1->val = 300;
+        }
+        ptr1 = ptr3;
+        ptr1->val = 400;
+        ptr1 = ptr2;
+        ptr1->val = 500;
+    }
+
+    void func6(Data1 *ptr1, Data1 *ptr3)
+    {
+        ptr1->val = 100;
+        ptr1 = tmp1;
+        tmp1->val = 200;
+        if (!ptr1)
+        {
+            ptr1->val = 300;
+        }
+        ptr1 = ptr3;
+        ptr1->val = 400;
+        ptr1 = tmp2;
+        ptr1->val = 500;
+    }
+
+    void func7(Data1 *ptr1, Data1 *ptr3)
+    {
+        Data1 *ptr2;
+        tmp1 = ptr3;
+        ptr1->val = 100;
+        ptr1 = ptr2;
+        ptr2->val = 200;
+        if (!ptr1)
+        {
+            ptr1->val = 300;
+        }
+        ptr1 = ptr3;
+        ptr1->val = 400;
+        ptr1 = tmp1;
+        ptr1->val = 500;
+    }
+
+    void func8(Data1 *ptr1, Data1 *ptr3)
+    {
+        Data1 *ptr2;
+        tmp1 = ptr3;
+        if (ptr1)
+        ptr1->val = 100;
+        ptr1 = ptr2;
+        ptr2->val = 200;
+        if (!ptr1)
+        {
+            ptr1->val = 300;
+        }
+        ptr1 = ptr3;
+        if (ptr3)
+            ptr1->val = 400;
+        ptr1 = tmp1;
+        ptr2 = ptr1;
+        if (tmp1)
+            ptr1->val = 500;
+        if (ptr2)
+            ptr1->val = 600;
+    }
 };
