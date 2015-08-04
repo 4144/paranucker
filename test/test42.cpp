6ac3241d5215d92077bde982db178c660ae6af8c
Andrei Karas <akaras@inbox.ru>
2015-08-04 18:44:55 +0300

    Add test 42.

---
+++ test/test42.cpp
@@ -0,0 +1,81 @@
+struct Data1;
+struct Data2;
+
+struct Data2
+{
+    int val;
+    Data1 *ptrval;
+    int arr[3];
+    Data1 *getData3()
+    {
+        return ptrval;
+    }
+};
+
+struct Data1
+{
+    int val;
+    Data1 *ptrval;
+    Data2 perm;
+    int arr[3];
+    virtual ~Data1();
+    virtual Data1 *getData3(int &k)
+    {
+        val = k;
+        return ptrval;
+    }
+    static Data1 *getData4()
+    {
+        return 0;
+    }
+    Data2 *getData5() __attribute__((returns_nonnull))
+    {
+        return &perm;
+    }
+};
+
+int k;
+
+Data1 *gptr1;
+Data1 *gptr2 __attribute__((nonnullpointer));
+
+void paranuckerInternalTest(const char *action, const char *collection, const char *values);
+
+class Object1
+{
+    Data1 *tmp1;
+    Data1 *tmp2;
+    Data2 *tmp3 __attribute__((nonnullpointer));
+
+    Data1 *getData1()
+    {
+        return tmp2;
+    }
+
+    Data1 *getData2()
+    {
+        return tmp2;
+    }
+
+    void func1(Data1 *ptr1) const
+    {
+        tmp1->val = 100;
+    }
+
+    void func2(Data1 *ptr1) const
+    {
+        tmp3->val = 100;
+    }
+
+/*
+    void func3(Data1 *ptr1) const
+    {
+        gptr1->val = 100;
+    }
+
+    void func4(Data1 *ptr1) const
+    {
+        gptr2->val = 100;
+    }
+*/
+};
